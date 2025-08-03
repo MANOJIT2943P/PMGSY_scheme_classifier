247fe9bc6110b53447e9b7cb8652c181c9015e27
MANOJIT2943P <118467865+MANOJIT2943P@users.noreply.github.com>
2025-08-03 18:07:38 +0530

    Endpoint changed to home from predict

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ model=joblib.load('Model/model.pkl')
 app=Flask(__name__)
 
 
-@app.route('/predict',methods=['GET','POST'])
-def predict():
+@app.route('/',methods=['GET','POST'])
+def home():
     if request.method=='GET':
         return render_template('index.html')
     else:
@@ -71,4 +71,4 @@ def predict():
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=5000)
