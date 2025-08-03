7e68039ff020b923e2185014c0cf6b58fd545033
MANOJIT2943P <118467865+MANOJIT2943P@users.noreply.github.com>
2025-08-03 16:58:35 +0530

    Major Changes

--- app.py
+++ app.py
@@ -1,15 +1,69 @@
 from flask import Flask,request,json,render_template
+import pandas as pd
 import joblib
 
 
-model=joblib.load('Model/P8.pickle')
+model=joblib.load('Model/model.pkl')
 
 app=Flask(__name__)
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/predict',methods=['GET','POST'])
 def predict():
-    return ''
+    if request.method=='GET':
+        return render_template('index.html')
+    else:
+        STATE_NAME=request.form['state']
+        DISTRICT_NAME=request.form['district']
+
+        NO_OF_ROAD_WORK_SANCTIONED=float(request.form['NO_OF_ROAD_WORK_SANCTIONED'])
+        LENGTH_OF_ROAD_WORK_SANCTIONED=float(request.form['LENGTH_OF_ROAD_WORK_SANCTIONED'])
+        NO_OF_BRIDGES_SANCTIONED=float(request.form['NO_OF_BRIDGES_SANCTIONED'])
+        COST_OF_WORKS_SANCTIONED=float(request.form['COST_OF_WORKS_SANCTIONED'])
+        NO_OF_ROAD_WORKS_COMPLETED=float(request.form['NO_OF_ROAD_WORKS_COMPLETED'])
+        
+        LENGTH_OF_ROAD_WORK_COMPLETED=float(request.form['LENGTH_OF_ROAD_WORK_COMPLETED'])
+        NO_OF_BRIDGES_COMPLETED=float(request.form['NO_OF_BRIDGES_COMPLETED'])
+        EXPENDITURE_OCCURED=float(request.form['EXPENDITURE_OCCURED'])
+        NO_OF_ROAD_WORKS_BALANCE=float(request.form['NO_OF_ROAD_WORKS_BALANCE'])
+        LENGTH_OF_ROAD_WORK_BALANCE=float(request.form['LENGTH_OF_ROAD_WORK_BALANCE'])
+        NO_OF_BRIDGES_BALANCE=float(request.form['NO_OF_BRIDGES_BALANCE'])
+
+        input=pd.DataFrame(
+            [[
+            STATE_NAME,
+            DISTRICT_NAME,
+            NO_OF_ROAD_WORK_SANCTIONED,
+            LENGTH_OF_ROAD_WORK_SANCTIONED,
+            NO_OF_BRIDGES_SANCTIONED,
+            COST_OF_WORKS_SANCTIONED,
+            NO_OF_ROAD_WORKS_COMPLETED,
+            LENGTH_OF_ROAD_WORK_COMPLETED,
+            NO_OF_BRIDGES_COMPLETED,
+            EXPENDITURE_OCCURED,
+            NO_OF_ROAD_WORKS_BALANCE,
+            LENGTH_OF_ROAD_WORK_BALANCE,
+            NO_OF_BRIDGES_BALANCE
+            ]],
+            columns=[
+            'STATE_NAME',
+            'DISTRICT_NAME',
+            'NO_OF_ROAD_WORK_SANCTIONED',
+            'LENGTH_OF_ROAD_WORK_SANCTIONED',
+            'NO_OF_BRIDGES_SANCTIONED',
+            'COST_OF_WORKS_SANCTIONED',
+            'NO_OF_ROAD_WORKS_COMPLETED',
+            'LENGTH_OF_ROAD_WORK_COMPLETED',
+            'NO_OF_BRIDGES_COMPLETED',
+            'EXPENDITURE_OCCURED',
+            'NO_OF_ROAD_WORKS_BALANCE',
+            'LENGTH_OF_ROAD_WORK_BALANCE',
+            'NO_OF_BRIDGES_BALANCE'
+            ]
+        )
+        result=model.predict(input)[0]
+
+        return render_template('index.html',ans=result)
 
 
 
